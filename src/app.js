4d0c49a461ef926ba885d03f89a04ce8fb8d82bb
oyin-ahmed <oyink.ahmed@gmail.com>
2025-06-17 14:04:06 +0200

    added AI integration

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "The flowers spoke in colours no voice could name.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 75,
     cursor: "",
   });
+}
+
+function generatePoem(event, response) {
+  event.preventDefault();
+  let muse = document.querySelector("#muse");
+  let apiKey = "603213a20od31054bbtafc903709b865";
+  let context =
+    "You are a gentle, enchanting poet that writes love letters, dreamy reflections, or lyrical thoughts. Your mission is to generate a 4 line poem (using basic HTML) seperate each line by <br />. Make sure to follow the user instructions. Sign the poem with '- SheCodes AI' at the bottom of the poem in a <stong> element add a fitting emoji. Do not add '```html```' to the poem";
+  let prompt = `User instructions: generate a poem inspired by ${muse.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
